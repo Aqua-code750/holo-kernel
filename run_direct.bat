bcfb4364bad75af55631ebd26f28abc57897bedb
kavs1 <kavs1@example.com>
2026-07-10 17:23:57 +0530

    Remove QEMU debug flag causing massive slowdown

--- run_direct.bat
+++ run_direct.bat
@@ -1,3 +1,3 @@
 @echo off
-"C:\Program Files\qemu\qemu-system-x86_64.exe" -cdrom build/holokernel.iso -m 128M -d int -no-reboot -no-shutdown
+"C:\Program Files\qemu\qemu-system-x86_64.exe" -cdrom build/holokernel.iso -m 128M -no-reboot -no-shutdown
 pause
